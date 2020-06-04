2a7d02a99288dd1c9d8bf6668c67a0671973327e
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2020-06-04 22:04:45 +0100

    Make test work at compile-time

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,6 +4,14 @@
 
 #include "chip8/chip8.h"
 
+template<bool test> bool static_test()
+{
+	static_assert(test);
+	return test;
+}
+
+#define TEST(x) static_test<x>()
+
 template<typename... T>
 constexpr auto run(T... program)
 {
@@ -18,12 +26,9 @@ constexpr auto run(T... program)
 TEST_CASE("00E0 resets all pixels", "[opcode]")
 {
 	constexpr auto emu = run(0xD0, 0x15, 0x00, 0xE0);
-	bool pixels_set = false;
 
-	for (auto y = 0; y < emu.display_height; ++y)
-		for (auto x = 0; x < emu.display_width; ++x)
-			if (emu.is_pixel_set(x, y))
-				pixels_set = true;
-
-	REQUIRE_FALSE(pixels_set);
+	REQUIRE(TEST(emu.is_pixel_set(0, 0) == false));
+	REQUIRE(TEST(emu.is_pixel_set(63, 0) == false));
+	REQUIRE(TEST(emu.is_pixel_set(0, 31) == false));
+	REQUIRE(TEST(emu.is_pixel_set(63, 31) == false));
 }
