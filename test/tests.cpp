f21cf5de83ecc9cd6959d7e06d7b58a480ab0f19
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2020-06-09 14:33:55 +0100

    Add test for 8XYE opcode

--- test/tests.cpp
+++ test/tests.cpp
@@ -176,3 +176,11 @@ TEST_CASE("8XY7 sets Vx to Vy minus Vx, settings Vf to 1 when there is not a bor
 	REQUIRE(TEST(emu.registers.data[0x0] == 1));
 	REQUIRE(TEST(emu.registers.data[0xF] == 1));
 }
+
+TEST_CASE("8XYE stores the most significant bit of Vx in Vf, and shifts Vx to the left by 1", "[opcode]")
+{
+	constexpr auto emu = run(0x60, 0xF3, 0x80, 0x1E);
+
+	REQUIRE(TEST(emu.registers.data[0x0] == 230));
+	REQUIRE(TEST(emu.registers.data[0xF] == 1));
+}
