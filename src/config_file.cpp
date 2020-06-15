32c9a44bf3d0adda42e993daefd96952de489428
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2020-06-15 21:11:34 +0100

    Create config file loader class

--- src/config_file.cpp
+++ src/config_file.cpp
@@ -1,2 +1,34 @@
 #include "config_file.h"
 
+#include <fstream>
+
+config_file::config_file(const std::string& file_path)
+{
+	load(file_path);
+}
+
+void config_file::load(const std::string& file_path)
+{
+	std::ifstream file;
+	file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try
+	{
+		file.open(file_path);
+
+		std::string line;
+		while (std::getline(file, line))
+		{
+			const auto delimiter_pos = line.find('=');
+
+			const auto key = line.substr(0, delimiter_pos);
+			const auto value = line.substr(delimiter_pos + 1, line.length());
+
+			m_config.insert({ key, value });
+		}
+	}
+	catch (const std::ifstream::failure&)
+	{
+		throw std::runtime_error("Failed to open \"" + file_path + "\"");
+	}
+}
--- src/config_file.h
+++ src/config_file.h
@@ -1,6 +1,65 @@
 #pragma once
 
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+
 class config_file
 {
+public:
+	config_file() = default;
+	config_file(const std::string& file_path);
 
+	void load(const std::string& file_path);
+
+	template<typename T>
+	inline std::optional<T> get_value(const std::string& key) const
+	{
+		try
+		{
+			return parse_value<T>(m_config.at(key));
+		}
+		catch (const std::logic_error& e)
+		{
+			return std::nullopt;
+		}
+	}
+
+private:
+	std::unordered_map<std::string, std::string> m_config;
+
+	template<typename T>
+	inline T parse_value(const std::string& value) const
+	{
+		return static_cast<T>(value);
+	}
+
+	template<typename T>
+	inline std::enable_if_t<std::is_floating_point_v<T>> parse_value(const std::string& value) const
+	{
+		return static_cast<T>(std::stold(value));
+	}
+
+	template<typename T>
+	inline std::enable_if_t<std::is_integral_v<T> && std::is_signed_v<T>> parse_value(const std::string& value) const
+	{
+		return static_cast<T>(std::stoll(value));
+	}
+
+	template<typename T>
+	inline std::enable_if_t<std::is_integral_v<T>&& std::is_unsigned_v<T>> parse_value(const std::string& value) const
+	{
+		return static_cast<T>(std::stoull(value));
+	}
+
+	template<>
+	inline bool parse_value<bool>(const std::string& value) const
+	{
+		if ((value != "false") && (value != "true"))
+			throw std::invalid_argument("Value must is not \"true\" or \"false\"");
+
+		return value == "true" ? true : false;
+	}
 };
